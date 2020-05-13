8d7160a415dea0a104252ffa35720285bb133352
ahoubouby <ahoubouby@gmail.com>
2020-05-13 01:51:35 +0000

    add job dsl for dockerBuildAndPublish

---
+++ jobdsl/nodejs-docker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS example') {
+    scm {
+        git('https://github.com/ahoubouby/nodejs_example') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('ahoubouby')
+            node / gitConfigEmail('ahoubouby@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+         dockerBuildAndPublish {
+            repositoryName('ahoubouby/nodejs-from-jobdsl')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
